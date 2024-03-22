3b62fbcd83a3f9c69709e7248e6558a39962d80e
KarlUlbaek <karl.ulbaek@hotmail.com>
2024-03-22 11:34:48 +0100

    cifar10 test in progress

--- code/src/s4_playground/seq_cifar10.py
+++ code/src/s4_playground/seq_cifar10.py
@@ -1,6 +1,100 @@
 import torchvision
+from torch.utils.data import DataLoader, Dataset
+import torch
+import tqdm
+from torch.optim import AdamW
+from torch.nn import CrossEntropyLoss
 
-root = "../data/cifar10/"
-trainset = torchvision.datasets.CIFAR10(root=root, train=True, download=True)
-testset = torchvision.datasets.CIFAR10(root=root, train=False, download=True)
+ROOT = "../data/cifar10/"
+
+class Cifar10seq(Dataset):
+   def __init__(self, train=True, d="cuda"):
+      data = torchvision.datasets.CIFAR10(root=ROOT, train=train, download=True)
+      x = torch.from_numpy(data.data)
+      x = torch.flatten(x, 1) / x.max()
+      self.x = torch.unsqueeze(x, -1).to(d)
+      self.y = torch.unsqueeze(torch.tensor(data.targets), -1).to(d)
+
+
+   def __len__(self):
+      return self.x.shape[0]
+
+   def __getitem__(self, idx):
+      return self.x[idx], self.y[idx]
+
+
+
+b = 64
+cifar_dataloader_train = DataLoader(dataset=Cifar10seq(train=True),
+                                    shuffle=True,
+                                    batch_size=b)
+cifar_dataloader_train = DataLoader(dataset=Cifar10seq(train=True),
+                                    shuffle=True,
+                                    batch_size=b)
+lr = 1e-4
+n_layers = 4
+d_data = 1
+d_model = 128
+d_state = 16
+L = next(iter(cifar_dataloader_train))[0].shape[1]
+d = "cuda"
+
+from mamba_fork.mamba_ssm.models.mixer_seq_simple import MixerModel as MambaNN
+s6NN = MambaNN(n_layer=n_layers, d_model=d_model, vocab_size=d_data, d_state=d_state, discrete=False, fused_add_norm=False).to(d)
+
+s4NN = MambaNN(n_layer=n_layers, d_model=d_model, vocab_size=d_data, d_state=d_state, discrete=False, fused_add_norm=False,
+               s4={"mode":"dplr", "hippo_init":"legs"}).to(d)
+s4dNN = MambaNN(n_layer=n_layers, d_model=d_model, vocab_size=d_data, d_state=d_state, discrete=False, fused_add_norm=False,
+               s4={"mode":"diag", "hippo_init":"legs"}).to(d)
+
+model = s4dNN
+#loss = CrossEntropyLoss()
+opt = AdamW(model.parameters(), lr=lr)
+
+
+n_epochs = 100
+
+for epoch_dix in tqdm.tqdm(range(n_epochs)):
+   for batch_idx, (x, y) in (enumerate(cifar_dataloader_train)):
+      pred = model(x)
+      last_pred = pred[:,-1,:]
+      loss = torch.nn.functional.cross_entropy(last_pred, y)
+      loss.backward()
+      opt.step()
+      opt.zero_grad()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
